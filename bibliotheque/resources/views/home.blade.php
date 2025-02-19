82190fa6b86785ab806f10ae576c618d371c30b2
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-02-19 15:34:46 +0100

    cretion du controller pour authentification

--- bibliotheque/resources/views/home.blade.php
+++ bibliotheque/resources/views/home.blade.php
@@ -36,7 +36,7 @@
             <div class="space-x-4">
                 <a href="#" class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded"
                     style="font-family: 'Sniglet', serif;">Register</a>
-                <a href="#" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+                <a href="/login" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
                     style="font-family: 'Sniglet', serif;">Login</a>
             </div>
 
--- bibliotheque/routes/web.php
+++ bibliotheque/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('home');
 });
- 
+
+// Routes d'authentification
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
